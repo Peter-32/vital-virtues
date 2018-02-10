56a8e4651565d6d18d6319719fe0cbb98d12bc85
Peter-32 <peter@impactradius.com>
2018-02-10 08:31:11 -0800

    _

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,7 +8,7 @@ class MainController < ApplicationController
     sql = "DELETE FROM weeks"
     ActiveRecord::Base.connection.execute(sql)
 
-    random_virtues = get_random_virtues
+    random_virtues = _get_random_virtues
 
     inserts = []
     time = Time.now.to_s(:db)
@@ -28,7 +28,7 @@ class MainController < ApplicationController
     redirect_to main_path
   end
 
-  def get_random_virtues
+  def _get_random_virtues
     virtues = ["Idealism","Creativity","Flexibility","Unity","Frugality","Temperance","Moderation","Tranquility","Humanity","Equanimity","Detachment","Determination","Discipline","Consistency","Industry","Reliable","Responsible","Excellence","Bravery","Resolution","Assertiveness","Honor","Confidence","Courage",
       "Obedience","Purity","Order","Cleanliness","Chastity","Humility","Justice","Modesty","Reverence","Respect","Hope","Joyfulness","Enthusiasm","Honesty","Truthfulness","Sincerity","Trustworthiness",
       "Faith","Prayerfulness","Wisdom","Kindness","Silence","Gentleness","Caring","Consideration","Courtesy","Tolerance","Faithfulness","Friendliness","Generosity","Service","Charity","Helpfulness","Patience","Forgiveness,","Mercy","Love","Trust","Thankfulness"]
@@ -37,37 +37,46 @@ class MainController < ApplicationController
   end
 
   def messed_up
-    correct_id, wday_name = find_today
-    sql = "UPDATE weeks SET #{wday_name} = 'X' WHERE id = #{correct_id}"
-    ActiveRecord::Base.connection.execute(sql)
-
+    _update_today_as('X')
+    redirect_to main_path
   end
 
   def undo
-    correct_id, wday_name = find_today
-    sql = "UPDATE weeks SET #{wday_name} = '' WHERE id = #{correct_id}"
-    ActiveRecord::Base.connection.execute(sql)
+    _update_today_as('')
+    redirect_to main_path
   end
 
-  def find_today
-    weeks = Week.find_by_sql("SELECT id, start_date FROM weeks ORDER BY id asc")
-    correct_id = nil
-    wday = nil
-    wday_name = nil
-    last_date = nil
+  def _update_today_as(new_value)
+    weeks = Week.find_by_sql("SELECT week_number, start_date FROM weeks ORDER BY id asc")
+    # correct_id = nil
+    # wday = nil
+    # wday_name = nil
+    # last_date = nil
     time = Time.now
+    puts time
     weeks.each do |week|
       last_date = week.start_date
       if (time < week.start_date)
-        correct_id = week.id - 1
-        wday = week.start_date - time
+        puts week.start_date
+        week_number = week.week_number.to_i - 1
+        puts week_number
+        wday = week.start_date.day - time.day
+        wday = 7 - wday
+        wday_name = _getWDayName(wday)
+        sql = "UPDATE weeks SET #{wday_name} = '#{new_value}' WHERE week_number = '#{week_number}'"
+        ActiveRecord::Base.connection.execute(sql)
+        return
       end
     end
-    if (correct_id = nil)
-      correct_id = 12
-      wday = last_date - time
-    end
+    week_number = 12
+    wday = last_date - time
+    wday_name = _getWDayName(wday)
+    sql = "UPDATE weeks SET #{wday_name} = '#{new_value}' WHERE week_number = '#{week_number}'"
+    ActiveRecord::Base.connection.execute(sql)
+  end
 
+  def _getWDayName(wday)
+    wday_name = nil
     if (wday == 0)
       wday_name = "sunday"
     elsif (wday == 1)
@@ -83,9 +92,7 @@ class MainController < ApplicationController
     elsif (wday == 6)
       wday_name = "saturday"
     end
-
-    return correct_id, wday_name
-
+    return wday_name
   end
 
 end
